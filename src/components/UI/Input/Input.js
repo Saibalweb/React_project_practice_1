daeb2078dbc30a9b4d492f4c2d515b9cbfc7faa9
Saibal kole <saibalkole@gmail.com>
2023-10-11 18:56:23 +0530

    uses context and Usecontext to send data directly from a components to another componenets

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,18 @@
+import classes from './Input.module.css'
+const Input = (props)=>{
+    return( <div
+        className={`${classes.control} ${
+          props.isValid === false ? classes.invalid : ''
+        }`}
+      >
+        <label htmlFor={props.email}>E-Mail</label>
+        <input
+          type={props.email}
+          id={props.id}
+          value={props.value}
+          onChange={props.emailChangeHandler}
+          onBlur={props.validateEmailHandler}
+        />
+      </div>);
+};
+export default Input;
